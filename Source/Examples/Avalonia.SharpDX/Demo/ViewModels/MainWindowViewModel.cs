222e7537ede75ec6938a209571a7328f5e0d0229
gulong <ch_gulong@163.com>
2026-06-12 17:41:48 +0800

    添加自用测试Demo

---
+++ Source/Examples/Avalonia.SharpDX/Demo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using HalconDotNet;
+using HelixToolkit;
+using HelixToolkit.SharpDX;
+using System.Numerics;
+
+namespace Demo.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+            using var model = new HObjectModel3D("C:\\Users\\Admin\\Desktop\\1.ply", new HTuple("m"),new HTuple(),new HTuple(), out _);
+            var xs = model.GetObjectModel3dParams("point_coord_x").ToFArr();
+            var ys = model.GetObjectModel3dParams("point_coord_y").ToFArr();
+            var zs = model.GetObjectModel3dParams("point_coord_z").ToFArr();
+            var vectors = new Vector3Collection(xs.Length);
+            for (int i = 0; i < xs.Length; i++)
+            {
+                vectors.Add(new Vector3(xs[i], ys[i], zs[i]));
+            }
+            PointCloud = new PointGeometry3D() { Positions = vectors };
+        }
+
+        public IEffectsManager EffectsManager { get; } = new DefaultEffectsManager();
+        public PointGeometry3D PointCloud { get; set; }
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
